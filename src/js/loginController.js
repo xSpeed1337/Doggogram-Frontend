33ba8a0957c31d9c529bf13208f6927f2c946bbc
D073534 <speedcrafter1@gmail.com>
2019-11-27 16:16:51 +0100

    Created User Login and Loginalerts

---
+++ src/js/loginController.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    let debug = true;
+
+    /**
+     * Login Method
+     */
+    $('#loginBtn').on('click', function () {
+        let loginAlert = '<div class="alert alert-warning alert-dismissible show" role="alert">\n' +
+            '                                Dieses feld darf <strong>NICHT</strong> leer sein! \n' +
+            '                                <button aria-label="Close" class="close" data-dismiss="alert" type="button">\n' +
+            '                                    <span aria-hidden="true">&times;</span>\n' +
+            '                                </button>\n' +
+            '                            </div>';
+        let loginUsername = $('#loginInputUsername').val();
+        let loginPasswort = $('#loginInputPassword').val();
+        let loginBool = true;
+        let loginJSON = {
+            user: loginUsername,
+            pass: loginPasswort
+        };
+
+        $(".alert").alert('close');
+
+        if (loginUsername == undefined || loginUsername == "") {
+            loginBool = false;
+            $('#loginInputUsername').after(loginAlert);
+        }
+
+        if (loginPasswort == undefined || loginPasswort == "") {
+            loginBool = false;
+            $('#loginInputPassword').after(loginAlert);
+        }
+
+        if (loginBool) {
+            $.ajax({
+                url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/auth/login',
+                contentType: "application/json",
+                type: 'POST',
+                data: JSON.stringify(loginJSON),
+                success: function (response) {
+                    if (debug) {
+                        console.log('succes: ' + JSON.stringify(response));
+                    }
+                    sessionStorage.setItem('token', response.token);
+                    sessionStorage.setItem('Username', loginUsername);
+                },
+                error: function (response) {
+                    if (debug) {
+                        console.log('error: ' + JSON.stringify(response));
+                    }
+                }
+            });
+        }
+    });
+
+    /**
+     * Register Method
+     */
+    $("#RegisterButton").on("click", function () {
+        let registerJSON = {
+            user: $('#exampleInputEmail1').val(),
+            pass: $('#exampleInputPassword1').val()
+        };
+
+        $.ajax({
+            url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/users/register/',
+            contentType: "application/json",
+            type: 'POST',
+            data: JSON.stringify(registerJSON),
+            success: function (response) {
+                if (debug) {
+                    console.log('succes: ' + JSON.stringify(response));
+                }
+            }
+        });
+    });
+
+});
