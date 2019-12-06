9da669962f3ccec59bc87de73b7de439a8fd0361
D073534 <speedcrafter1@gmail.com>
2019-12-06 17:26:25 +0100

    Reworked Image Loading Process

--- src/js/feedController.js
+++ src/js/feedController.js
@@ -1,55 +1,59 @@
+let token = sessionStorage.getItem('token');
+let scrollLoad = true;
+let feedImageID = 0;
+let feedLastImageID = 0;
+
 $(document).ready(function () {
 
-    let token = sessionStorage.getItem('token');
-    let scrollLoad = true;
-    let feedImageID = 0;
-    let feedLastImageID = 0;
-
+    loadFeedImage();
 
     /**
      * Loads new Content when User reaches the end of the Window
      */
     $(window).scroll(function () {
         if (scrollLoad && $(window).scrollTop() >= $(document).height() - $(window).height() - 300) {
-
-            scrollLoad = false;
-
-            $.ajax({
-                url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/images/discover/' + feedImageID,
-                type: 'GET',
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                },
-                success: function (response) {
-                    for (let i = 0; i < response.imageDTOS.length; i++) {
-                        let imageDiv = "<div class=\"post-container\" style=\"flex-direction: column\">\n" +
-                            "                    <article class=\"post\">\n" +
-                            "                        <header class=\"bd-post-title\">\n" +
-                            "                            <img alt=\"\" class=\"bd-post-pp\" src=\"../resources/images/superthumb.jpg\">\n" +
-                            "                            <span class=\"bd-post-name\">Mein Name</span>\n" +
-                            "                        </header>\n" +
-                            "                        <div class=\"bd-post-img-container\">\n" +
-                            "                            <img id=\"image" + response.imageDTOS[i].id + "\" alt=\"\" class=\"bd-post-img\" src=\"\data:image/jpeg;base64," + response.imageDTOS[i].image + "\">\n" +
-                            "                        </div>\n" +
-                            "                        <div class=\"bd-post-stats\">\n" +
-                            "                            <a class=\"bd-post-favtext\"><i class=\"material-icons bd-post-favicon\">favorite</i><span\n" +
-                            "                                    class=\"bd-post-span\">" + response.imageDTOS[i].likes + "</span></a>\n" +
-                            "                            <a class=\"bd-post-chattext\"><i class=\"material-icons bd-post-chaticon\">chat</i><span\n" +
-                            "                                    class=\"bd-post-span\">" + response.imageDTOS[1].comments + "</span></a>\n" +
-                            "                        </div>\n" +
-                            "                    </article>\n" +
-                            "                </div>";
-
-                        $('#feedContainer').append(imageDiv);
-                        feedLastImageID = response.imageDTOS[i].id;
-                    }
-                    if (feedImageID != feedLastImageID) {
-                        feedImageID = feedLastImageID;
-                        scrollLoad = true;
-                    }
-                }
-            });
+            loadFeedImage();
         }
     });
 
 });
+
+function loadFeedImage() {
+    scrollLoad = false;
+
+    $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/images/discover/' + feedImageID,
+        type: 'GET',
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        success: function (response) {
+            for (let i = 0; i < response.imageDTOS.length; i++) {
+                let imageDiv = "<div class=\"post-container\" style=\"flex-direction: column\">\n" +
+                    "                    <article class=\"post\">\n" +
+                    "                        <header class=\"bd-post-title\">\n" +
+                    "                            <img alt=\"\" class=\"bd-post-pp\" src=\"../resources/images/superthumb.jpg\">\n" +
+                    "                            <span class=\"bd-post-name\">Mein Name</span>\n" +
+                    "                        </header>\n" +
+                    "                        <div class=\"bd-post-img-container\">\n" +
+                    "                            <img id=\"image" + response.imageDTOS[i].id + "\" alt=\"\" class=\"bd-post-img\" src=\"\data:image/jpeg;base64," + response.imageDTOS[i].image + "\">\n" +
+                    "                        </div>\n" +
+                    "                        <div class=\"bd-post-stats\">\n" +
+                    "                            <a class=\"bd-post-favtext\"><i class=\"material-icons bd-post-favicon\">favorite</i><span\n" +
+                    "                                    class=\"bd-post-span\">" + response.imageDTOS[i].likes + "</span></a>\n" +
+                    "                            <a class=\"bd-post-chattext\"><i class=\"material-icons bd-post-chaticon\">chat</i><span\n" +
+                    "                                    class=\"bd-post-span\">" + response.imageDTOS[1].comments + "</span></a>\n" +
+                    "                        </div>\n" +
+                    "                    </article>\n" +
+                    "                </div>";
+
+                $('#feedContainer').append(imageDiv);
+                feedLastImageID = response.imageDTOS[i].id;
+            }
+            if (feedImageID != feedLastImageID) {
+                feedImageID = feedLastImageID;
+                scrollLoad = true;
+            }
+        }
+    });
+}
