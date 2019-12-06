a62c02311ecef8eaaed2b400af45a164a3794d69
D073534 <speedcrafter1@gmail.com>
2019-12-06 13:35:01 +0100

    Feed V1

--- src/js/feedController.js
+++ src/js/feedController.js
@@ -1,29 +1,34 @@
 $(document).ready(function () {
     let token = sessionStorage.getItem('token');
-    let imageDiv = "<div class=\"post-container\" style=\"flex-direction: column\">\n" +
-        "                    <article class=\"post\">\n" +
-        "                        <header class=\"bd-post-title\">\n" +
-        "                            <img class=\"bd-post-pp\" src=\"../resources/images/superthumb.jpg\" alt=\"\">\n" +
-        "                            <span class=\"bd-post-name\">Mein Name</span>\n" +
-        "                        </header>\n" +
-        "                        <div class=\"bd-post-img-container\">\n" +
-        "                            <img id='image' class=\"bd-post-img\" alt=\"\">\n" +
-        "                        </div>\n" +
-        "                        <div class=\"bd-post-stats\">\n" +
-        "                            <a class=\"bd-post-favtext\"><i class=\"material-icons bd-post-favicon\">favorite</i><span class=\"bd-post-span\">1000</span></a>\n" +
-        "                            <a class=\"bd-post-chattext\"><i class=\"material-icons bd-post-chaticon\">chat</i><span class=\"bd-post-span\">1000</span></a>\n" +
-        "                        </div>\n" +
-        "                    </article>\n" +
-        "                </div>"
 
     $.ajax({
-        url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/images/all',
+        url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/discover/feed/0',
         type: 'GET',
         headers: {
             "Authorization": `Bearer ${token}`
         },
         success: function (response) {
+            for (let i = 0; i < response.imageDTOS.length; i++) {
+                let imageDiv = "<div class=\"post-container\" style=\"flex-direction: column\">\n" +
+                    "                    <article class=\"post\">\n" +
+                    "                        <header class=\"bd-post-title\">\n" +
+                    "                            <img alt=\"\" class=\"bd-post-pp\" src=\"../resources/images/superthumb.jpg\">\n" +
+                    "                            <span class=\"bd-post-name\">Mein Name</span>\n" +
+                    "                        </header>\n" +
+                    "                        <div class=\"bd-post-img-container\">\n" +
+                    "                            <img id=\"image" + response.imageDTOS[i].id + "\" alt=\"\" class=\"bd-post-img\" src=\data:image/png;base64" + response.imageDTOS[i].image + "\">\n" +
+                    "                        </div>\n" +
+                    "                        <div class=\"bd-post-stats\">\n" +
+                    "                            <a class=\"bd-post-favtext\"><i class=\"material-icons bd-post-favicon\">favorite</i><span\n" +
+                    "                                    class=\"bd-post-span\">" + response.imageDTOS[i].likes + "</span></a>\n" +
+                    "                            <a class=\"bd-post-chattext\"><i class=\"material-icons bd-post-chaticon\">chat</i><span\n" +
+                    "                                    class=\"bd-post-span\">" + response.imageDTOS[1].comments.length + "</span></a>\n" +
+                    "                        </div>\n" +
+                    "                    </article>\n" +
+                    "                </div>";
 
+                $('#feedContainer').append(imageDiv);
+            }
         }
     });
 });
