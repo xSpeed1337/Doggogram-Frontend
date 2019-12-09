dab182e6260e803c5632eee3ecf177962a624a27
moritzbuerkle <buerkle.moritz@web.de>
2019-12-09 23:40:18 +0100

    settings controller change pw

---
+++ src/js/settingsController.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+let token = sessionStorage.getItem('token');
+let debug = true;
+
+$("#idChangePassword").on('click', function () {
+
+    let oldPw = $("#idInputOldPassword").val();
+    let newPw = $("#idInputNewPassword").val();
+ pwformdata.append('oldPassword', oldPw);
+pwformdata.append('newPassword', newPw);
+
+        $.ajax({
+            url: 'https://cors-anywhere.herokuapp.com/http://88.214.57.214:6889/api/v1/users/update/password',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            data: pwformdata,
+            headers: {
+                "Authorization": `Bearer ${token}`
+            },
+            success: function (response) {
+                if (debug === true) {
+                    console.log('succes: ' + JSON.stringify(response));
+                }
+
+            },
+            error: function (response) {
+                if (debug === true) {
+                    console.log('error: ' + JSON.stringify(response));
+                }
+            }
+        });
+
+});
+
+});
